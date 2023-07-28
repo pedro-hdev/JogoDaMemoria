4465f56d2a6b27a8114de063ea3ecb2cf5818d69
pedro-hdev <pedrophveras@gmail.com>
2023-07-28 00:04:23 -0300

    add component FontHeader

---
+++ app-game/src/app/components/FontHeader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+function FontHeader({texto}) {
+    return (
+       
+            <h1 className="text-[white] text-[24px]">{texto}</h1>
+        
+    )
+}
+export default FontHeader;
