a7224a585e2f7d4d35a0eb7e925db410febf76b4
pedro-hdev <pedrophveras@gmail.com>
2023-08-04 14:27:21 -0300

    create button

---
+++ app-game/src/components/buttonwhite.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Buttonwhite ({texto}){
+  return (
+    <button className="font-Righteous bg-[#fff] mt-[30px] p-[6px] text-[24px] rounded-bl-[30px] rounded-tr-[30px] w-[260px]">{texto}</button>
+
+    
+  )
+
+
+}
+export default Buttonwhite;
