2606969376f3f7d46c45ab6122c5a09bf566145f
pedro-hdev <pedrophveras@gmail.com>
2023-08-04 14:24:41 -0300

    create page login, added component input,buttonwhite

--- app-game/src/app/page.js
+++ app-game/src/app/page.js
@@ -1,9 +1,23 @@
+import Buttonwhite from "@/components/buttonwhite";
+import Input from "@/components/input";
+import Link from "next/link"
+import Register from "./register";
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>dhgsah</h1>
-      
-    </main>
+    <div className="flex justify-center items-center text-center p-[8px] flex-col">
+      <form className=" pt-[100px] flex flex-col justify-center items-center">
+        <h1 className="font-Righteous text-[24px] text-[white] text-center">LOGIN</h1>
+        <label className="pt-[50px]">
+          <Input placeholder={"LOGIN"} type={"text"}></Input>
+
+        </label>
+        <label className="pt-[20px]">
+          <Input placeholder={"SENHA"} type={"password"}></Input>
+        </label>
+        <Buttonwhite texto="ENTRAR"></Buttonwhite>
+      </form>
+      <p>Não tem uma conta? <a href="">Registre-se aqui.</a></p>
+    </div>
   )
 }
