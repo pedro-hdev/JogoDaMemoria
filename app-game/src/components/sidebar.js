4c2bb1258582f5554856da46752f0f62e7705c2d
patinhoBlue <pedrophveras@gmail.com>
2023-08-04 19:39:25 -0300

    create sidebar put a table

---
+++ app-game/src/components/sidebar.js
@@ -0,0 +1,106 @@
+import React from "react";
+
+function Sidebar({}) {
+  return (
+    <div className="font-Righteous bg-[white] h-[100%]">
+      <div className="flex text-[24px] p-[20px] gap-[70px]">
+        <h1>Erros</h1>
+        <div className="rounded-full bg-[black] w-[39px] h-[39px] text-center">
+          <p className="text-[white]">1</p>
+        </div>
+      </div>
+      <hr></hr>
+      <div className="flex text-[24px] p-[20px] gap-[40px]">
+        <h1>Ranking</h1>
+        <div className="rounded-full bg-[black] w-[39px] h-[39px] text-center">
+          <p className="text-[white]">1</p>
+        </div>
+      </div>
+      <table className="w-[100%]">
+        <tbody className="w-[100%]">
+          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
+            <td className="text-[18px]">User</td>
+            <td>
+              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
+                <p className="text-[white]">1</p>
+              </div>
+            </td>
+          </tr>
+          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
+            <td className="text-[18px]">User</td>
+            <td>
+              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
+                <p className="text-[white]">1</p>
+              </div>
+            </td>
+          </tr>
+          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
+            <td className="text-[18px]">User</td>
+            <td>
+              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
+                <p className="text-[white]">1</p>
+              </div>
+            </td>
+          </tr>
+          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
+            <td className="text-[18px]">User</td>
+            <td>
+              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
+                <p className="text-[white]">1</p>
+              </div>
+            </td>
+          </tr>
+          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
+            <td className="text-[18px]">User</td>
+            <td>
+              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
+                <p className="text-[white]">1</p>
+              </div>
+            </td>
+          </tr>
+          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
+            <td className="text-[18px]">User</td>
+            <td>
+              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
+                <p className="text-[white]">1</p>
+              </div>
+            </td>
+          </tr>
+          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
+            <td className="text-[18px]">User</td>
+            <td>
+              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
+                <p className="text-[white]">1</p>
+              </div>
+            </td>
+          </tr>
+          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
+            <td className="text-[18px]">User</td>
+            <td>
+              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
+                <p className="text-[white]">1</p>
+              </div>
+            </td>
+          </tr>
+          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
+            <td className="text-[18px]">User</td>
+            <td>
+              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
+                <p className="text-[white]">1</p>
+              </div>
+            </td>
+          </tr>
+          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
+            <td className="text-[18px]">User</td>
+            <td>
+              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
+                <p className="text-[white]">1</p>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+export default Sidebar;
