910db1ec180561074d33b99b37b0c7238d17c588
pedro-hdev <pedrophveras@gmail.com>
2023-08-04 14:28:05 -0300

    create menu

---
+++ app-game/src/components/menu.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function Menu({ logolink, textolink1, textolink2, logo, texto1, texto2 }) {
+    return (
+
+        <div className="font-Righteous flex bg-black p-3 justify-between text-[white] text-[10px] font-[400] tracking-[2px] sm:text-[16px] lg:text-[24px]">
+            <a href={logolink}>{logo}</a>
+            <div className="flex gap-3">
+                <a href={textolink1}>{texto1}</a>
+                <a href={textolink2}>{texto2}</a>
+
+
+            </div>
+        </div>
+
+
+    )
+}
+export default Menu;
