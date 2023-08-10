994e3cbcfe477702c9ca21005814d620ce2e82a2
pedro-hdev <pedrophveras@gmail.com>
2023-08-10 00:18:38 -0300

    added: new table

--- app-game/src/components/sidebar.js
+++ app-game/src/components/sidebar.js
@@ -1,6 +1,20 @@
-import React from "react";
+'use client'
 
-function Sidebar({}) {
+import React, { useState, useEffect } from "react";
+
+function Sidebar() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+
+    setData([{ nome: "Pedro", pontos: "1", }, { nome: "Kayo", pontos: "2", }, { nome: "Bruno", pontos: "3", }]);
+
+
+  }, []);
+
+
+
   return (
     <div className="font-Righteous bg-[white] h-[100%]">
       <div className="flex text-[24px] p-[20px] gap-[70px]">
@@ -18,86 +32,18 @@ function Sidebar({}) {
       </div>
       <table className="w-[100%]">
         <tbody className="w-[100%]">
-          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
-            <td className="text-[18px]">User</td>
-            <td>
-              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
-                <p className="text-[white]">1</p>
-              </div>
-            </td>
-          </tr>
-          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
-            <td className="text-[18px]">User</td>
-            <td>
-              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
-                <p className="text-[white]">1</p>
-              </div>
-            </td>
-          </tr>
-          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
-            <td className="text-[18px]">User</td>
-            <td>
-              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
-                <p className="text-[white]">1</p>
-              </div>
-            </td>
-          </tr>
-          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
-            <td className="text-[18px]">User</td>
-            <td>
-              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
-                <p className="text-[white]">1</p>
-              </div>
-            </td>
-          </tr>
-          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
-            <td className="text-[18px]">User</td>
-            <td>
-              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
-                <p className="text-[white]">1</p>
-              </div>
-            </td>
-          </tr>
-          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
-            <td className="text-[18px]">User</td>
-            <td>
-              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
-                <p className="text-[white]">1</p>
-              </div>
-            </td>
-          </tr>
-          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
-            <td className="text-[18px]">User</td>
-            <td>
-              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
-                <p className="text-[white]">1</p>
-              </div>
-            </td>
-          </tr>
-          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
-            <td className="text-[18px]">User</td>
-            <td>
-              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
-                <p className="text-[white]">1</p>
-              </div>
-            </td>
-          </tr>
-          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
-            <td className="text-[18px]">User</td>
-            <td>
-              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
-                <p className="text-[white]">1</p>
-              </div>
-            </td>
-          </tr>
-          <tr className="flex justify-between w-[100%] px-[30px] py-[5px]">
-            <td className="text-[18px]">User</td>
-            <td>
-              <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
-                <p className="text-[white]">1</p>
-              </div>
-            </td>
-          </tr>
+          {data.map((value, index) => (
+
+            <tr key={index} className="flex justify-between w-[100%] px-[30px] py-[5px]">
+              <td className="text-[18px]">{value.nome}</td>
+              <td>
+                <div className="rounded-full bg-[black] w-[23px] h-[23px] text-center">
+                  <p className="text-[white]">{value.pontos}</p>
+                </div>
+              </td>
+            </tr>
+          ))}
+
         </tbody>
       </table>
     </div>
